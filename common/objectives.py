920df186190f61716e5e69751fc1ff7b55a0185e
lxh5147 <lxh5147@gmail.com>
2016-08-30 17:22:47 +0800

    rnn search model refactored

--- common/objectives.py
+++ common/objectives.py
@@ -5,7 +5,7 @@ Created on Jul 19, 2016
 '''
 import keras.backend as K
 
-def categorical_crossentropy_ex(y_true, y_pred):
+def categorical_crossentropy_ex(y_true, y_pred, from_logits=False):
     '''
     cross entropy (i.e., - log-likelihood) of the ground truth based on current prediction.
 
@@ -14,10 +14,11 @@ def categorical_crossentropy_ex(y_true, y_pred):
     y_true : binary tensor
     y_pred: a tensor of float, must have the same shape of y_true
         each element of y_pred is a float between [0,1], and the sum of y_pred along the last dimension is a ONE tensor
+    from_logits: y_pred represents the unscaled log probabilities
 
     # Returns
     ------
     - log-likelihood of y_true under y_pred
     '''
-    return K.sum(K.categorical_crossentropy(y_pred, y_true))
+    return K.sum(K.categorical_crossentropy(y_pred, y_true, from_logits))
 
--- mt/rnnsearch_model.py
+++ mt/rnnsearch_model.py
@@ -6,18 +6,28 @@ NMT translation model
 
 from keras.models import Model
 from keras.layers import Input, GRU, Embedding, Dense
+import keras.backend as K
 
-from ..common import  BiDirectionalLayer, AttentionLayer, RNNDecoderLayer, RNNDecoderLayerWithBeamSearch, MLPClassifierLayer, TimeDistributed, categorical_crossentropy_ex
+from ..common import get_k_best_from_lattice, BiDirectionalLayer, AttentionLayer, RNNDecoderLayer, RNNDecoderLayerWithBeamSearch, MLPClassifierLayer, TimeDistributed, categorical_crossentropy_ex
+import numpy as np
 
+def build_rnn_search_model(source_vacabuary_size,
+                           source_embedding_dim,
+                           source_initia_embedding,
+                           encoder_rnn_output_dim_list,
+                           attention_context_dim,
+                           decoder_rnn_output_dim,
+                           target_vacabuary_size,
+                           target_embedding_dim,
+                           target_initia_embedding,
+                           decoder_hidden_unit_numbers,
+                           decoder_hidden_unit_activation_functions,
+                           optimizer='rmsprop',
+                           beam_search_max_output_length,
+                           beam_size):
 
-def build_rnn_search_model(source_vacabuary_size, source_embedding_dim, source_initia_embedding,
-                 encoder_rnn_output_dim_list,
-                 attention_context_dim,
-                 decoder_rnn_output_dim,
-                 target_vacabuary_size, target_embedding_dim, target_initia_embedding,
-                 decoder_hidden_unit_numbers, decoder_hidden_unit_activation_functions,
-                 optimizer='rmsprop',
-                 beam_search_max_output_length, beam_size):
+    # TODO: support weight regularizer
+
     source_word = Input((None,), dtype='int32')
     # source_word = trim_right_padding(source_word)
     source_embedding = Embedding(source_vacabuary_size, source_embedding_dim, weights=[source_initia_embedding])
@@ -56,7 +66,8 @@ def build_rnn_search_model(source_vacabuary_size, source_embedding_dim, source_i
 
 
     rnn_search_model = Model(input=[source_word, decoder_input_sequence], output=time_distributed_mlp_classifier_output)
-    # TODO: try advanced loss function based on negative sampling
+    # TODO: try other loss, such as importance sampling based loss, e.g., sampled_softmax_loss (this will need to extend Keras model, which assumes that the loss function does not hold any trainable parameters
+
     rnn_search_model.compile(optimizer=optimizer, loss=categorical_crossentropy_ex, metrics=['accuracy'])
 
     beam_search_initial_input = Input(get_shape=(1,))
@@ -66,3 +77,87 @@ def build_rnn_search_model(source_vacabuary_size, source_embedding_dim, source_i
     rnn_search_runtime_model = Model(input=[source_word, beam_search_initial_input], output=beam_search_output_lattice)
 
     return (rnn_search_model, rnn_search_runtime_model)
+
+def train(rnn_search_model,
+          generator,
+          samples_per_epoch,
+          nb_epoch,
+          verbose=1,
+          callbacks=[],
+          validation_data=None,
+          nb_val_samples=None,
+          class_weight={},
+          max_q_size=10,
+          nb_worker=1,
+          pickle_safe=False):
+
+    return rnn_search_model.fit_generator(generator,
+                                          samples_per_epoch, nb_epoch,
+                                          verbose,
+                                          callbacks,
+                                          validation_data, nb_val_samples,
+                                          class_weight,
+                                          max_q_size, nb_worker, pickle_safe)
+
+def _build_predict_func(rnn_search_runtime_model, eos=None):
+    if rnn_search_runtime_model.uses_learning_phase:
+        inputs = rnn_search_runtime_model.inputs + [K.learning_phase()]
+    else:
+        inputs = rnn_search_runtime_model.inputs
+    lattice = rnn_search_runtime_model.outputs
+    outputs = get_k_best_from_lattice(lattice, k=1, eos=eos)
+    return K.function(inputs, outputs)
+
+def _predict_loop(model, f, ins, batch_size=32, verbose=0):
+    nb_sample = len(ins[0])
+    outs = []
+    if verbose == 1:
+        progbar = model.Progbar(target=nb_sample)
+    batches = model.make_batches(nb_sample, batch_size)
+    index_array = np.arange(nb_sample)
+    for _, (batch_start, batch_end) in enumerate(batches):
+        batch_ids = index_array[batch_start:batch_end]
+        if type(ins[-1]) is float:
+            # do not slice the training phase flag
+            ins_batch = model.slice_X(ins[:-1], batch_ids) + [ins[-1]]
+        else:
+            ins_batch = model.slice_X(ins, batch_ids)
+
+        batch_outs = f(ins_batch)
+        outs += batch_outs
+        if verbose == 1:
+            progbar.update(batch_end)
+
+    return outs
+
+def predict(rnn_search_runtime_model, x, bos, batch_size=32, verbose=1, eos=None):
+    f = _build_predict_func(rnn_search_runtime_model, eos)
+    # get all outputs
+    initial_input_val = [bos for _ in range(len(x))]
+    if rnn_search_runtime_model.uses_learning_phase:
+        ins = x + initial_input_val + [0.]
+    else:
+        ins = x + initial_input_val
+
+    outputs_val = _predict_loop(rnn_search_runtime_model,
+                          f,
+                          ins, batch_size,
+                          verbose)
+
+    # nb_samples, k, time_steps -> nb_samples, time_steps
+    outputs_val = np.squeeze(outputs_val, axis=(1,))
+    return outputs_val
+
+def evaluate(rnn_search_runtime_model, x, y, bos, batch_size=32, verbose=1, sample_weight=None, eos=None):
+    outputs_val = predict(rnn_search_runtime_model, x, bos, batch_size, verbose, eos)
+    # calculate BLEU on multiple reference
+    return _calc_bleu(outputs_val, y, sample_weight, eos)
+
+def _calc_bleu(predict, reference, sample_weight=None, eos=None):
+    # predict: nb_samples, time_steps, reference
+    # reference nb_samples, nb_references, time_steps
+    # sample_weight: None or nb_samples
+    # TODO: implement BLEU
+    pass
+
+
