3111a2205c5b1d479825cc8458fc36afc7505b51
JinXinDeep <JinXinDeep@users.noreply.github.com>
2017-06-24 20:55:49 +0800

    Create gen.py

---
+++ gen.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2016 Matthew Earl
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+#     The above copyright notice and this permission notice shall be included
+#     in all copies or substantial portions of the Software.
+# 
+#     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+#     OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#     MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+#     NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+#     DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+#     OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+#     USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+
+"""
+Generate training and test images.
+
+"""
+
+
+__all__ = (
+    'generate_ims',
+)
+
+
+import itertools
+import math
+import os
+import random
+import sys
+
+import cv2
+import numpy
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+import common
+
+
+OUTPUT_SHAPE = (64, 128)
+
+CHARS = common.CHARS + " "
+
+
+
+def euler_to_mat(yaw, pitch, roll):
+    # Rotate clockwise about the Y-axis
+    c, s = math.cos(yaw), math.sin(yaw)
+    M = numpy.matrix([[  c, 0.,  s],
+                      [ 0., 1., 0.],
+                      [ -s, 0.,  c]])
+
+    # Rotate clockwise about the X-axis
+    c, s = math.cos(pitch), math.sin(pitch)
+    M = numpy.matrix([[ 1., 0., 0.],
+                      [ 0.,  c, -s],
+                      [ 0.,  s,  c]]) * M
+
+    # Rotate clockwise about the Z-axis
+    c, s = math.cos(roll), math.sin(roll)
+    M = numpy.matrix([[  c, -s, 0.],
+                      [  s,  c, 0.],
+                      [ 0., 0., 1.]]) * M
+
+    return M
+
+
+
+
+def make_affine_transform(from_shape, to_shape, 
+                          min_scale, max_scale,
+                          scale_variation=1.0,
+                          rotation_variation=1.0,
+                          translation_variation=1.0):
+    out_of_bounds = False
+
+    from_size = numpy.array([[from_shape[1], from_shape[0]]]).T
+    to_size = numpy.array([[to_shape[1], to_shape[0]]]).T
+
+    scale = random.uniform((min_scale + max_scale) * 0.5 -
+                           (max_scale - min_scale) * 0.5 * scale_variation,
+                           (min_scale + max_scale) * 0.5 +
+                           (max_scale - min_scale) * 0.5 * scale_variation)
+    if scale > max_scale or scale < min_scale:
+        out_of_bounds = True
+    roll = random.uniform(-0.1, 0.1) * rotation_variation
+    pitch = random.uniform(-0.1, 0.1) * rotation_variation
+    yaw = random.uniform(-0.2, 0.2) * rotation_variation
+
+    # Compute a bounding box on the skewed input image (`from_shape`).
+    M = euler_to_mat(yaw, pitch, roll)[:2, :2]
+    h, w = from_shape
+    corners = numpy.matrix([[-w, +w, -w, +w],
+                            [-h, -h, +h, +h]]) * 0.5
+    skewed_size = numpy.array(numpy.max(M * corners, axis=1) -
+                              numpy.min(M * corners, axis=1))
+
+    # Set the scale as large as possible such that the skewed and scaled shape
+    # is less than or equal to the desired ratio in either dimension.
+    scale *= numpy.min(to_size / skewed_size)
+
+    # Set the translation such that the skewed and scaled image falls within
+    # the output shape's bounds.
+    trans = (numpy.random.random((2,1)) - 0.5) * translation_variation
+    trans = ((2.0 * trans) ** 5.0) / 2.0
+    if numpy.any(trans < -0.5) or numpy.any(trans > 0.5):
+        out_of_bounds = True
+    trans = (to_size - skewed_size * scale) * trans
+
+    center_to = to_size / 2.
+    center_from = from_size / 2.
+
+    M = euler_to_mat(yaw, pitch, roll)[:2, :2]
+    M *= scale
+    M = numpy.hstack([M, trans + center_to - M * center_from])
+
+    return M, out_of_bounds
+
+
+
+
+
+def rounded_rect(shape, radius):
+    out = numpy.ones(shape)
+    out[:radius, :radius] = 0.0
+    out[-radius:, :radius] = 0.0
+    out[:radius, -radius:] = 0.0
+    out[-radius:, -radius:] = 0.0
+
+    cv2.circle(out, (radius, radius), radius, 1.0, -1)
+    cv2.circle(out, (radius, shape[0] - radius), radius, 1.0, -1)
+    cv2.circle(out, (shape[1] - radius, radius), radius, 1.0, -1)
+    cv2.circle(out, (shape[1] - radius, shape[0] - radius), radius, 1.0, -1)
+
+    return out
+
+
+
+def generate_bg(mydir):    
+    classes = os.listdir(mydir)    
+    nrof_classes = len(classes)
+    tmpidx=random.randint(0, nrof_classes - 1)
+    fname = os.path.join(mydir,classes[tmpidx])
+    bg = cv2.imread(fname) / 255.
+    return bg
+
+def generate_plate(mydir):
+    classes = os.listdir(mydir)    
+    nrof_classes = len(classes)
+    tmpidx=random.randint(0, nrof_classes - 1)
+    fname = os.path.join(mydir,classes[tmpidx])
+    my_image = cv2.imread(fname) / 255.
+
+    return my_image/255., numpy.ones(my_image.shape)
+  
+
+def generate_im(id_img_dir, bg_img_dir):
+    bg = generate_bg(bg_img_dir)
+
+    plate, plate_mask = generate_plate(id_img_dir)
+    
+    M, out_of_bounds = make_affine_transform(
+                            from_shape=plate.shape,
+                            to_shape=bg.shape,
+                            min_scale=0.6,
+                            max_scale=0.875,
+                            rotation_variation=1.0,
+                            scale_variation=1.5,
+                            translation_variation=1.2)
+    plate = cv2.warpAffine(plate, M, (bg.shape[1], bg.shape[0]))
+    plate_mask = cv2.warpAffine(plate_mask, M, (bg.shape[1], bg.shape[0]))
+
+    out = plate * plate_mask + bg * (1.0 - plate_mask)
+
+    out = cv2.resize(out, (OUTPUT_SHAPE[1], OUTPUT_SHAPE[0]))
+
+    out += numpy.random.normal(scale=0.05, size=out.shape)
+    out = numpy.clip(out, 0., 1.)
+
+    return out, code, not out_of_bounds
+
+
+
+
+
+def generate_ims():
+    """
+    Generate number plate images.
+
+    :return:
+        Iterable of number plate images.
+
+    """
+    variation = 1.0
+    num_bg_images = len(os.listdir("bgs")) 
+    while True:
+        yield generate_im(font_char_ims[random.choice(fonts)], num_bg_images)
+
+
+if __name__ == "__main__":
+    os.mkdir("test")
+    im_gen = itertools.islice(generate_ims(), int(sys.argv[1]))
+    for img_idx, (im, c, p) in enumerate(im_gen):
+        fname = "test/{:08d}_{}_{}.png".format(img_idx, c,
+                                               "1" if p else "0")
+        print fname
+        cv2.imwrite(fname, im * 255.)
+
